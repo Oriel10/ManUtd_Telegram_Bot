44c10e7127ad108537cd60ba7f25bed0e72af25b
Oriel10 <orielav10@gmail.com>
2022-10-10 01:24:39 +0300

    add Constans file

---
+++ Constants.py
@@ -0,0 +1,6 @@
+API_KEY = "5542999003:AAFoaBSxmFklsV-8ooleMdT3UVrWEena39s"
+
+football_headers = {
+	"X-RapidAPI-Key": "f86b7ad105msh8c38ba6202f5d8dp1fb4f0jsn8fccb00a2d20",
+	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+}

03a38d526bc2c44d7b4d9d81df03a0dc3eff4c95
Oriel10 <orielav10@gmail.com>
2022-10-10 02:26:03 +0300

    commit

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from urllib import response
 import Constants as constants
 from telegram.ext import *
 import Responses as R
-from footballdata import *
+from footballdata import getNextFixturesList, getPrevFixturesList
 
 print("Bot started...")
+
+def checkInt(my_str):
+    if my_str[0] in ('-', '+'):
+        return my_str[1:].isdigit()
+    return my_str.isdigit()
 
 def start_command(update, context):
     update.message.reply_text("Type something random to get started!")
@@ -48,8 +53,8 @@ def main():
 
     dp.add_handler(CommandHandler("start", start_command))
     dp.add_handler(CommandHandler("help", help_command))
-    # dp.add_handler(CommandHandler("next_i", nexti_command, pass_args=True))
-    # dp.add_handler(CommandHandler("prev_i", previ_command, pass_args=True))
+    dp.add_handler(CommandHandler("next_i", nexti_command, pass_args=True))
+    dp.add_handler(CommandHandler("prev_i", previ_command, pass_args=True))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
